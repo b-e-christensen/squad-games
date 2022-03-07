1142e535ace9a2aa31b996189c979217dedae692
Ben Christensen <ben.eric.christensen@gmail.com>
2022-03-07 14:16:06 -0700

    added a lot of the init function. Main focus for now should be on generated html

--- index.js
+++ index.js
@@ -9,3 +9,138 @@
 5. Repeat steps 3 - 4a until user exits. 
 6. Generate HTML. 
 */
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+let engineerNum = 1
+
+//everyone has id name and email
+generalQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is this employee\'s ID?'
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is thier name?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is thier email?'
+        },
+    ])
+}
+//manager question: office number
+managerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is their officeNumber?'
+        }
+    ])
+}
+// engineer question : github
+engineerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is their GitHub?'
+        }
+    ])
+}
+//intern question: school
+internQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school do they attend?'
+        }
+    ])
+}
+
+addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'newEmployee',
+            message: 'Would you like to add another employee?',
+            choices: [ 'Engineer', 'Intern', 'No' ]
+        }
+    ])
+}
+
+employeeType = (answers) => {
+    if(answers.newEmployee === 'Engineer'){
+        addEngineer()
+    } else if (answers.newEmployee === 'Intern'){
+        addIntern()
+    } else {
+        process.exit()
+    }
+}
+
+function init() {
+    let manager = new Manager
+    generalQuestions()
+        .then((answers) => {
+            manager.id = answers.id;
+            manager.name = answers.name;
+            manager.email = answers.email
+        })
+        .then(() => managerQuestions())
+        .then((answers) => manager.officeNumber = answers.officeNumber)
+        .then(() => addEmployee())
+        .then((answers) => employeeType(answers))
+}
+
+function addEngineer() {
+    let engineer = new Engineer
+    generalQuestions()
+        .then((answers) => {
+            engineer.id = answers.id;
+            engineer.name = answers.name;
+            engineer.email = answers.email
+        })
+        .then(() => engineerQuestions())
+        .then((answers) => engineer.github = answers.github)
+        .then(() => addEmployee())
+        .then((answers) => employeeType(answers))
+  }
+
+  function addIntern() {
+    let intern = new Intern
+    generalQuestions()
+        .then((answers) => {
+            intern.id = answers.id;
+            intern.name = answers.name;
+            intern.email = answers.email
+        })
+        .then(() => internQuestions())
+        .then((answers) => intern.school = answers.school)
+        .then(() => addEmployee())
+        .then((answers) => employeeType(answers))
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+init()
