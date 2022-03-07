889f72d57c9810b725b257b244b2c7f0e69c0021
Ben Christensen <ben.eric.christensen@gmail.com>
2022-03-06 17:20:43 -0700

    added data flow breakdown

---
+++ index.js
@@ -0,0 +1,11 @@
+// PSEUDO TIME //
+// Data flow ~~ 
+/* 
+1. node command to initiate app 
+2. Prompt user function to run through questions on info for employee. (input example of inquirer)
+3. Ask user if they want to create info for another employee. 
+4. If no -- process.exit()
+4a. If yes, run the prompts for that specific employee.  
+5. Repeat steps 3 - 4a until user exits. 
+6. Generate HTML. 
+*/
